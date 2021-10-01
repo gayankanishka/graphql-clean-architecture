7686843f129e6da2b1a9cc2dd94d041c23542279
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-10-01 22:49:02 +0530

    feat: add common models

---
+++ src/Domain/Common/Payload.cs
@@ -0,0 +1,12 @@
+namespace ConferencePlanner.Domain.Common
+{
+    public abstract class Payload
+    {
+        protected Payload(IReadOnlyList<UserError>? errors = null)
+        {
+            Errors = errors;
+        }
+
+        public IReadOnlyList<UserError>? Errors { get; }
+    }
+}
---
+++ src/Domain/Common/UserError.cs
@@ -0,0 +1,4 @@
+namespace ConferencePlanner.Domain.Common
+{
+    public record UserError(string Message, string Code);
+}
