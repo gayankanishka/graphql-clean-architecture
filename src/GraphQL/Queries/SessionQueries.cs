30986f238c7f9f686c1755300f4d6f8d2768b420
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-10-01 22:48:17 +0530

    feat: add queries

---
+++ src/GraphQL/Queries/SessionQueries.cs
@@ -0,0 +1,31 @@
+using ConferencePlanner.GraphQL.Sessions;
+using HotChocolate;
+using HotChocolate.Types;
+using HotChocolate.Types.Relay;
+
+namespace ConferencePlanner.GraphQL.Queries
+{
+    [ExtendObjectType(OperationTypeNames.Query)]
+    public class SessionQueries
+    {
+        [UseApplicationDbContext]
+        [UsePaging]
+        [UseFiltering(typeof(SessionFilterInputType))]
+        [UseSorting]
+        public IQueryable<Session> GetSessions(
+            [ScopedService] ApplicationDbContext context) 
+            => context.Sessions;
+
+        public Task<Session> GetSessionByIdAsync(
+            [ID(nameof(Session))] int id,
+            SessionByIdDataLoader sessionById,
+            CancellationToken cancellationToken) 
+            => sessionById.LoadAsync(id, cancellationToken);
+
+        public async Task<IEnumerable<Session>> GetSessionsByIdAsync(
+            [ID(nameof(Session))] int[] ids,
+            SessionByIdDataLoader sessionById,
+            CancellationToken cancellationToken) 
+            => await sessionById.LoadAsync(ids, cancellationToken);
+    }
+}
