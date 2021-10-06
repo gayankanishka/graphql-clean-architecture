6a0711a7ed3466a6167d2a231335d5ee9296c9f0
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-10-06 23:00:19 +0530

    Update AttendeeNode.cs

--- src/GraphQL/Nodes/AttendeeNode.cs
+++ src/GraphQL/Nodes/AttendeeNode.cs
@@ -1,6 +1,9 @@
+using ConferencePlanner.Application.Attendees.Queries.GetAttendeeById;
+using ConferencePlanner.Domain.Entities;
 using HotChocolate;
 using HotChocolate.Types;
 using HotChocolate.Types.Relay;
+using MediatR;
 
 namespace ConferencePlanner.GraphQL.Nodes
 {
@@ -8,26 +11,26 @@ namespace ConferencePlanner.GraphQL.Nodes
     [ExtendObjectType]
     public class AttendeeNode
     {
-        public async Task<IEnumerable<Session>> GetSessionsAsync(
-            [Parent] Attendee attendee,
-            [ScopedService] ApplicationDbContext dbContext,
-            SessionByIdDataLoader sessionById,
-            CancellationToken cancellationToken)
-        {
-            int[] speakerIds = await dbContext.Attendees
-                .Where(a => a.Id == attendee.Id)
-                .Include(a => a.SessionsAttendees)
-                .SelectMany(a => a.SessionsAttendees.Select(t => t.SessionId))
-                .ToArrayAsync(cancellationToken);
-
-            return await sessionById.LoadAsync(speakerIds, cancellationToken);
-        }
+        // public async Task<IEnumerable<Session>> GetSessionsAsync(
+        //     [Parent] Attendee attendee,
+        //     [ScopedService] ApplicationDbContext dbContext,
+        //     SessionByIdDataLoader sessionById,
+        //     CancellationToken cancellationToken)
+        // {
+        //     int[] speakerIds = await dbContext.Attendees
+        //         .Where(a => a.Id == attendee.Id)
+        //         .Include(a => a.SessionsAttendees)
+        //         .SelectMany(a => a.SessionsAttendees.Select(t => t.SessionId))
+        //         .ToArrayAsync(cancellationToken);
+        //
+        //     return await sessionById.LoadAsync(speakerIds, cancellationToken);
+        // }
 
         [NodeResolver]
         public static Task<Attendee> GetAttendeeAsync(
-            int id,
-            AttendeeByIdDataLoader attendeeById,
+            GetAttendeeByIdQuery input,
+            [Service] IMediator mediator,
             CancellationToken cancellationToken)
-            => attendeeById.LoadAsync(id, cancellationToken);
+            => mediator.Send(input, cancellationToken);
     }
 }
