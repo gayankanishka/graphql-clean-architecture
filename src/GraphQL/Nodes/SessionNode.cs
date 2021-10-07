3a46ee85ea43f3889677e6fb0a81de29a1f6772b
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-10-07 21:26:12 +0530

    enable session resolver

--- src/GraphQL/Nodes/SessionNode.cs
+++ src/GraphQL/Nodes/SessionNode.cs
@@ -1,6 +1,10 @@
+using ConferencePlanner.Application.Sessions.Queries.GetSessionById;
+using ConferencePlanner.Application.Tracks.Queries.GetTrackById;
+using ConferencePlanner.Domain.Entities;
 using HotChocolate;
 using HotChocolate.Types;
 using HotChocolate.Types.Relay;
+using MediatR;
 
 namespace ConferencePlanner.GraphQL.Nodes
 {
@@ -8,36 +12,36 @@ namespace ConferencePlanner.GraphQL.Nodes
     [ExtendObjectType(typeof(Session))]
     public class SessionNode
     {
-        [BindMember(nameof(Session.SessionSpeakers), Replace = true)]
-        public Task<Speaker[]> GetSpeakersAsync(
-            [Parent] Session session,
-            SpeakerBySessionIdDataLoader speakerBySessionId,
-            CancellationToken cancellationToken)
-            => speakerBySessionId.LoadAsync(session.Id, cancellationToken);
+        // [BindMember(nameof(Session.SessionSpeakers), Replace = true)]
+        // public Task<Speaker[]> GetSpeakersAsync(
+        //     [Parent] Session session,
+        //     SpeakerBySessionIdDataLoader speakerBySessionId,
+        //     CancellationToken cancellationToken)
+        //     => speakerBySessionId.LoadAsync(session.Id, cancellationToken);
 
-        [UsePaging(ConnectionName = "SessionAttendees")]
-        [BindMember(nameof(Session.SessionAttendees), Replace = true)]
-        public IQueryable<Attendee> GetAttendees(
-            [Parent] Session session,
-            [ScopedService] ApplicationDbContext dbContext)
-            => dbContext.Sessions
-                .Where(s => s.Id == session.Id)
-                .Include(s => s.SessionAttendees)
-                .SelectMany(s => s.SessionAttendees.Select(t => t.Attendee!));
+        // [UsePaging(ConnectionName = "SessionAttendees")]
+        // [BindMember(nameof(Session.SessionAttendees), Replace = true)]
+        // public IQueryable<Attendee> GetAttendees(
+        //     [Parent] Session session,
+        //     [ScopedService] ApplicationDbContext dbContext)
+        //     => dbContext.Sessions
+        //         .Where(s => s.Id == session.Id)
+        //         .Include(s => s.SessionAttendees)
+        //         .SelectMany(s => s.SessionAttendees.Select(t => t.Attendee!));
 
         public async Task<Track?> GetTrackAsync(
             [Parent] Session session,
-            TrackByIdDataLoader trackById,
+            [Service] IMediator mediator,
             CancellationToken cancellationToken)
             => session.TrackId is not null
-                ? await trackById.LoadAsync(session.TrackId.Value, cancellationToken)
+                ? await mediator.Send(new GetTrackByIdQuery(session.TrackId.Value), cancellationToken)
                 : null;
 
         [NodeResolver]
         public static Task<Session> GetSessionByIdAsync(
-            int id,
-            SessionByIdDataLoader sessionById,
+            GetSessionByIdQuery input,
+            [Service] IMediator mediator,
             CancellationToken cancellationToken)
-            => sessionById.LoadAsync(id, cancellationToken);
+            => mediator.Send(input, cancellationToken);
     }
 }
