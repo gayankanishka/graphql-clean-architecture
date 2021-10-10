d2e92aef3293f11d347f374e14743756cd8f3d3d
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-10-10 15:41:08 +0530

    feat: import new methods from parent repo

--- src/GraphQL/Nodes/SpeakerNode.cs
+++ src/GraphQL/Nodes/SpeakerNode.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using ConferencePlanner.Application.Sessions.Queries.GetSessionsBySpeakerId;
 using ConferencePlanner.Application.Speakers.Queries.GetSpeakerById;
 using ConferencePlanner.Domain.Entities;
@@ -19,6 +20,44 @@ namespace ConferencePlanner.GraphQL.Nodes
             CancellationToken cancellationToken)
             => await mediator.Send(new GetSessionsBySpeakerIdQuery(speaker.Id), cancellationToken);
 
+        public async Task<IEnumerable<Session>> GetSessionsExpensiveAsync(
+            [Parent] Speaker speaker,
+            [Service] IMediator mediator,
+            CancellationToken cancellationToken)
+        {
+            await Task.Delay(new Random().Next(1000, 3000), cancellationToken);
+
+            return await mediator.Send(new GetSessionsBySpeakerIdQuery(speaker.Id), cancellationToken);
+        }
+
+        // public async IAsyncEnumerable<Session> GetSessionsStreamAsync(
+        //     [Parent] Speaker speaker,
+        //     [Service] IMediator mediator,
+        //     [EnumeratorCancellation] CancellationToken cancellationToken)
+        // {
+        //     var random = new Random();
+        //
+        //     await Task.Delay(random.Next(500, 1000), cancellationToken);
+        //
+        //     await using var context = contextFactory.CreateDbContext();
+        //
+        //     var stream = (IAsyncEnumerable<SessionSpeaker>)context.Speakers
+        //         .Where(s => s.Id == speaker.Id)
+        //         .Include(s => s.SessionSpeakers)
+        //         .SelectMany(s => s.SessionSpeakers)
+        //         .Include(s => s.Session);
+        //
+        //     await foreach (var item in stream.WithCancellation(cancellationToken))
+        //     {
+        //         if (item.Session is not null)
+        //         {
+        //             yield return item.Session;
+        //         }
+        //
+        //         await Task.Delay(random.Next(100, 300), cancellationToken);
+        //     }
+        // }
+        
         [NodeResolver]
         public static Task<Speaker> GetSpeakerByIdAsync(
             GetSpeakerByIdQuery input,
